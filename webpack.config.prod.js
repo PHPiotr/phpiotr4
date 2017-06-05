3d31b2e53fed710db2a6c43e9ab923ed8c2a157a
Piotr Kowalski <piet.kowalski@gmail.com>
2017-06-05 22:42:35 +0100

    webpack config prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,33 @@
-var config = require('./webpack.config'),
-    webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
 
-config.plugins.push(new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}));
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+var config = {
+    context: path.resolve(__dirname, 'app'),
+    devtool: 'inline-source-map',
+    entry: {
+        app: [
+            './AppContainer',
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[hash].js',
+        publicPath: '/',
+    },
+    module: {
+        rules: []
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, 'node_modules')
+        ]),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor'],
+        })
+    ]
+};
 
 module.exports = config;
+
