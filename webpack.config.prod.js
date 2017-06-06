4717f6ee84258f1bea4568f874c6798f15669a18
Piotr Kowalski <piet.kowalski@gmail.com>
2017-06-06 17:10:26 +0100

    Webpack not found

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 var config = {
