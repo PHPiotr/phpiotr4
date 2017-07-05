d2e3393696f044dc2d333a41fc75ba9db1e61180
Piotr Kowalski <piet.kowalski@gmail.com>
2017-07-05 01:34:12 +0100

    Remove unused dependencies. Clean package.json

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 var express = require('express');
 var static = require('serve-static');
-var methodOverride = require('method-override');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var flash = require('connect-flash');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var config = require('./config.js');
 
 var app = express();
 var server = require('http').Server(app);
 
-// view engine setup
-app.set('view engine', 'pug');
 app.set('superSecret', process.env.AUTH_SECRET);
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(methodOverride('_method'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
 if (process.env.NODE_ENV !== 'production') {
     var webpack = require('webpack');
