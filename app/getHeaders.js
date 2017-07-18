0ce10f07c287ccffc23b728df3b7e11f3a992c9f
Piotr Kowalski <piet.kowalski@gmail.com>
2017-07-18 22:45:52 +0100

    Fix headers. Missing content type

--- app/getHeaders.js
+++ app/getHeaders.js
@@ -3,9 +3,14 @@ import cookie from 'cookie-monster';
 const getHeaders = () => {
     let token = cookie.getItem(process.env.TOKEN_KEY);
     if (token) {
-        return {'Authorization': `Bearer ${token}`};
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        };
     }
-    return {};
+    return {
+        'Content-Type': 'application/json'
+    };
 };
 
 export default getHeaders;
