80ace0cc21c5f11060d02c2517741e205928984a
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-06 23:28:02 +0000

    Compose with dev tools

--- app/configureStore.js
+++ app/configureStore.js
@@ -3,22 +3,24 @@ import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const configureStore = () => {
     const middlewares = [thunk];
-
-    if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(createLogger());
+    if (process.env.NODE_ENV === 'production') {
+        return createStore(
+            reducers,
+            applyMiddleware(...middlewares)
+        );
     }
-
+    middlewares.push(createLogger());
     const store = createStore(
         reducers,
-        applyMiddleware(...middlewares)
+        composeWithDevTools(applyMiddleware(...middlewares))
     );
-
     if (module.hot) {
         module.hot.accept('./reducers', () => {
-            const nextRootReducer = require('./reducers/index').default;
+            const nextRootReducer = require('./reducers/index');
             store.replaceReducer(nextRootReducer);
         });
     }
