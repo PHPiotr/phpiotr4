70ca6778ec7f2ff0043c6c9ba9c21a9bc0e240b2
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-11 05:24:57 +0000

    Refactor pagination

--- app/components/presentation/Pagination.js
+++ app/components/presentation/Pagination.js
@@ -1,39 +1,42 @@
 import React from 'react';
-import NavLink from '../nav/NavLink';
+import BottomNavigation, {BottomNavigationButton} from 'material-ui/BottomNavigation';
 
 const Pagination = (props) => {
 
     if (props[props.bookingsLabel].data === undefined) {
         return null;
     }
-
-    let bookings = props[props.bookingsLabel].data;
-
-    if (bookings.pages_count <= 1) {
+    const bookings = props[props.bookingsLabel].data || {};
+    if (!bookings.pages_count || bookings.pages_count <= 1) {
         return null;
     }
-
-    let pages_count = bookings.pages_count;
-    let active = bookings.active;
-    let pages_counter = [];
+    const pages_count = bookings.pages_count;
+    const active = bookings.active;
+    const pages_counter = [];
     for (var i = 1; i <= pages_count; i++) {
         pages_counter.push(i);
     }
-    let pages = pages_counter.map(page => (
-        <NavLink current={bookings.current_page === page} className={bookings.is_first_page && page === 1 ? 'active' : ''}
-            onClick={props.fetchBookings.bind(this, active, page)}
-            key={`pagination-${props.bookingsLabel}-${active}-${page}`}
-            to={`/bookings/${props.bookingsLabel}/${active}/${page}`}>{page}</NavLink>
+    const pages = pages_counter.map(page => (
+        <BottomNavigationButton key={page} label={page} href={`/bookings/${props.bookingsLabel}/${active}/${page}`}/>
     ));
 
+    const handleOnChange = (event, value) => {
+        event.preventDefault();
+        props.fetchBookings(active, value + 1);
+        const url = `/bookings/${props.bookingsLabel}/${active}/${value + 1}`;
+        if (props.history.location.pathname !== url) {
+            props.history.push(url);
+        }
+    };
+
     return (
-        <div className="row-fluid">
-            <nav aria-label="Page navigation">
-                <ul className="pagination pagination-sm">
-                    {pages}
-                </ul>
-            </nav>
-        </div>
+        <BottomNavigation
+            value={bookings.current_page - 1}
+            onChange={handleOnChange}
+            showLabels
+        >
+            {pages}
+        </BottomNavigation>
     );
 };
 
