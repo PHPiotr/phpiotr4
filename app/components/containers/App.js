87cf352374045d7ee27afe9d599482a4a86290df
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-12 15:11:14 +0000

    Get rid of Card wrapping content

--- app/components/containers/App.js
+++ app/components/containers/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import Navbar from '../presentation/Navbar';
-import Card from 'material-ui/Card';
 
 const App = ({children}) => [
     <Navbar key={1}/>,
-    <Card key={2} style={{paddingTop: 70}}>{children && React.cloneElement(children, {})}</Card>,
+    <div key={2} style={{paddingTop: 70}}>{children && React.cloneElement(children, {})}</div>,
 ];
 
 export default withRouter(connect()(App));
