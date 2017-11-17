f3870abd2b9aca4afe4e0617962395fe483e663a
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-17 04:06:08 +0000

    Add icons to menu items

--- app/components/presentation/Navbar.js
+++ app/components/presentation/Navbar.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import {AppBar, Drawer, Typography, Toolbar} from 'material-ui';
 import {toggleIsDrawerOpen} from '../../actions/app/appActions';
-import List, {ListItem} from 'material-ui/List';
+import List, {ListItem, ListItemIcon} from 'material-ui/List';
 import {connect} from 'react-redux';
 import {withStyles} from 'material-ui/styles';
 import Divider from 'material-ui/Divider';
 import ToolbarIconButtonLeft from './ToolbarIconButtonLeft';
+import ReportIcon from 'material-ui-icons/Home';
+import BusIcon from 'material-ui-icons/DirectionsBus';
+import PlaneIcon from 'material-ui-icons/Flight';
+import TrainIcon from 'material-ui-icons/Train';
+import HostelIcon from 'material-ui-icons/Hotel';
+import SignInIcon from 'material-ui-icons/Input';
+import SignUpIcon from 'material-ui-icons/PersonAdd';
+import SignoutIcon from 'material-ui-icons/ExitToApp';
 
 const styles = ({palette}) => ({
-    list: {
-        width: 250,
-    },
     listFull: {
         width: 'auto',
     },
@@ -36,19 +41,43 @@ const Navbar = (props) => {
                     <div className={classes.list} onClick={props.toggleIsDrawerOpen}>{
                         !props.isLoggedIn ? (
                             <List>
-                                <ListItem button><Link className={classes.link} to={'/login'}>Login</Link></ListItem>
-                                <ListItem button><Link className={classes.link} to={'/register'}>Register</Link></ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><SignInIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/login'}>Sign in</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><SignUpIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/register'}>Sign up</Link>
+                                </ListItem>
                             </List>
                         ) : (
                             <List>
-                                <ListItem button><Link className={classes.link} to={'/'}>Report</Link></ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><ReportIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/'}>Report</Link>
+                                </ListItem>
                                 <Divider/>
-                                <ListItem button><Link className={classes.link} to={'/bookings/buses'}>Buses</Link></ListItem>
-                                <ListItem button><Link className={classes.link} to={'/bookings/planes'}>Planes</Link></ListItem>
-                                <ListItem button><Link className={classes.link} to={'/bookings/trains'}>Trains</Link></ListItem>
-                                <ListItem button><Link className={classes.link} to={'/bookings/hostels'}>Hostels</Link></ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><BusIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/bookings/buses'}>Buses</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><PlaneIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/bookings/planes'}>Planes</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><TrainIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/bookings/trains'}>Trains</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><HostelIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/bookings/hostels'}>Hostels</Link>
+                                </ListItem>
                                 <Divider/>
-                                <ListItem button><Link className={classes.link} to={'/logout'}>Logout</Link></ListItem>
+                                <ListItem button>
+                                    <ListItemIcon><SignoutIcon/></ListItemIcon>
+                                    <Link className={classes.link} to={'/logout'}>Sign out</Link>
+                                </ListItem>
                             </List>
                         )
                     }</div>
