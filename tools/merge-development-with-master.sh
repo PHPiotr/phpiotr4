19c020cade7ef0dc94aa85bdfb7cb09cde7236d9
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-19 04:08:58 +0000

    Merge development with master after successful build - change committer

--- tools/merge-development-with-master.sh
+++ tools/merge-development-with-master.sh
@@ -5,12 +5,12 @@ if [ "$TRAVIS_BRANCH" != "development" ]; then
   exit 0;
 fi
 
-git config --global user.email $GITHUB_EMAIL
-git config --global user.name $GITHUB_USERNAME
+git config --global user.email $TRAVIS_COMMITTER_EMAIL
+git config --global user.name $TRAVIS_COMMITTER_NAME
 git remote set-branches --add origin master
 git fetch
 git reset --hard
 git checkout master --force
 git merge --no-ff --no-edit development
-git remote add travis-origin https://${GITHUB_SECRET_TOKEN}@github.com/PHPiotr/phpiotr4
-git push --quiet --set-upstream travis-origin master
+git remote add travis-origin https://${GITHUB_TOKEN}@github.com/PHPiotr/phpiotr4
+git push --set-upstream travis-origin master
