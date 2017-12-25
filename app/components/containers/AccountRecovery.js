73483f28fa2c3d2cf88f0d63cc2f6775b448eaa2
Piotr Kowalski <piet.kowalski@gmail.com>
2017-12-25 15:00:38 +0000

    If is logged in,don't show account recovery page

--- app/components/containers/AccountRecovery.js
+++ app/components/containers/AccountRecovery.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import {FormControl} from 'material-ui/Form';
@@ -10,16 +11,20 @@ class AccountRecovery extends Component {
     handleSubmit = () => {};
 
     render() {
+        if (this.props.isLoggedIn) {
+            return null;
+        }
+
         return (
             <Fragment>
                 <Typography style={{padding: '23px'}} type="headline">Forgot your account's password?</Typography>
                 <form style={{padding: '20px'}} onSubmit={this.handleSubmit}>
                     <FormControl component="fieldset">
                         <TextField
-                            helperText={'Enter your email address and we\'ll send you a recovery link.'}
-                            id={'username'}
-                            type={'text'}
-                            name={'username'}
+                            helperText="Enter your email address and we'll send you a recovery link."
+                            id={'email'}
+                            type={'email'}
+                            name={'email'}
                             onChange={this.handleChange}
                             onFocus={this.handleFocus}
                             value={''}
@@ -32,4 +37,10 @@ class AccountRecovery extends Component {
     }
 }
 
-export default NoAuth(AccountRecovery);
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.auth.isLoggedIn,
+    };
+};
+
+export default NoAuth(connect(mapStateToProps)(AccountRecovery));
