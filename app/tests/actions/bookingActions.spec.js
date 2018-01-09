4bea2b28962008ea67937d37c78636d6ae6645e2
Piotr Kowalski <piet.kowalski@gmail.com>
2018-01-09 14:47:35 +0000

    It should not start deleting booking if the process already in progress

--- app/tests/actions/bookingActions.spec.js
+++ app/tests/actions/bookingActions.spec.js
@@ -192,6 +192,26 @@ describe('bookingActions', () => {
                 .then(() => expect(store.getActions()).toEqual([]));
         });
 
+        it(`should not create ${bookingActionTypes.DELETE_BOOKING_REQUEST} when booking is already being deleted`, () => {
+            store = mockStore({
+                bookings: {
+                    [pluralToSingularMapping[label]]: {
+                        isDeleting: true,
+                    },
+                    currentBooking: {
+                        labelPlural: label,
+                        label: pluralToSingularMapping[label],
+                        id,
+                    },
+                },
+                auth: {
+                    token: 'j.w.t',
+                },
+            });
+            return store.dispatch(bookingActions.deleteBookingIfNeeded())
+                .then(() => expect(store.getActions()).toEqual([]));
+        });
+
         it(`should create ${bookingActionTypes.GET_BOOKING_SUCCESS} when viewing of booking succeeded`, () => {
             nock(apiUrl).get(`${apiPrefix}/bookings/${label}/${id}`).reply(200, booking);
             const expectedRequestAction = {
