096d72c659949f7bde9be2376267d9d9feb4b1e2
Piotr Kowalski <piet.kowalski@gmail.com>
2018-01-22 13:04:02 +0000

    Refactor - bus form styling

--- app/components/presentation/BusForm.js
+++ app/components/presentation/BusForm.js
@@ -2,74 +2,101 @@ import React from 'react';
 import Button from 'material-ui/Button';
 import {FormControl, FormGroup, FormControlLabel} from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
-import TextField from 'material-ui/TextField';
 import {NEW_BUS, EDIT_BUS} from '../../constants';
 import Booking from '../containers/Booking';
+import Input, {InputLabel} from 'material-ui/Input';
+import {withStyles} from 'material-ui/styles';
 
-const BusForm = ({handleSubmit, handleChange, handleFocus, bus}) => {
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+    },
+});
+
+const BusForm = (props) => {
+
+    const {bus} = props;
+
     return (
-        <form style={{padding: '20px'}} onSubmit={handleSubmit}>
+        <form className={props.classes.root} onSubmit={props.handleSubmit}>
             <FormControl component="fieldset">
-                <TextField
-                    error={bus.errors.booking_number && !!bus.errors.booking_number.message}
-                    helperText={`Code: ${(bus.errors.booking_number && !!bus.errors.booking_number.message) ? bus.errors.booking_number.message : ''}`}
-                    id={'booking-number'}
-                    type={'text'}
-                    name={'booking_number'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.booking_number || ''}
-                />
-                <TextField
-                    error={bus.errors.from && !!bus.errors.from.message}
-                    helperText={`From: ${(bus.errors.from && !!bus.errors.from.message) ? bus.errors.from.message : ''}`}
-                    id={'from'}
-                    type={'text'}
-                    name={'from'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.from || ''}
-                />
-                <TextField
-                    error={bus.errors.to && !!bus.errors.to.message}
-                    helperText={`To: ${(bus.errors.to && !!bus.errors.to.message) ? bus.errors.to.message : ''}`}
-                    id={'to'}
-                    type={'text'}
-                    name={'to'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.to || ''}
-                />
-                <TextField
-                    error={bus.errors.departure_date && !!bus.errors.departure_date.message}
-                    helperText={`Departure date: ${(bus.errors.departure_date && !!bus.errors.departure_date.message) ? bus.errors.departure_date.message : ''}`}
-                    id={'departure-date'}
-                    type={'date'}
-                    name={'departure_date'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.departure_date || ''}
-                />
-                <TextField
-                    error={bus.errors.departure_time && !!bus.errors.departure_time.message}
-                    helperText={`Departure time: ${(bus.errors.departure_time && !!bus.errors.departure_time.message) ? bus.errors.departure_time.message : ''}`}
-                    id={'departure-time'}
-                    type={'time'}
-                    name={'departure_time'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.departure_time || ''}
-                />
-                <TextField
-                    error={bus.errors.price && !!bus.errors.price.message}
-                    helperText={`Price: ${(bus.errors.price && !!bus.errors.price.message) ? bus.errors.price.message : ''}`}
-                    id={'price'}
-                    type={'text'}
-                    name={'price'}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
-                    value={bus.current.price || '0.00'}
-                />
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`Code: ${(bus.errors.booking_number && !!bus.errors.booking_number.message) ? bus.errors.booking_number.message : ''}`}</InputLabel>
+                    <Input
+                        id={'booking-number'}
+                        type={'text'}
+                        name={'booking_number'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.booking_number || ''}
+                        error={bus.errors.booking_number && !!bus.errors.booking_number.message}
+                    />
+                </FormControl>
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`From: ${(bus.errors.from && !!bus.errors.from.message) ? bus.errors.from.message : ''}`}</InputLabel>
+                    <Input
+                        id={'from'}
+                        type={'text'}
+                        name={'from'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.from || ''}
+                        error={bus.errors.from && !!bus.errors.from.message}
+                    />
+                </FormControl>
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`To: ${(bus.errors.to && !!bus.errors.to.message) ? bus.errors.to.message : ''}`}</InputLabel>
+                    <Input
+                        id={'to'}
+                        type={'text'}
+                        name={'to'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.to || ''}
+                        error={bus.errors.to && !!bus.errors.to.message}
+                    />
+                </FormControl>
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`Departure date: ${(bus.errors.departure_date && !!bus.errors.departure_date.message) ? bus.errors.departure_date.message : ''}`}</InputLabel>
+                    <Input
+                        id={'departure-date'}
+                        type={'date'}
+                        name={'departure_date'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.departure_date || ''}
+                        error={bus.errors.departure_date && !!bus.errors.departure_date.message}
+                    />
+                </FormControl>
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`Departure time: ${(bus.errors.departure_time && !!bus.errors.departure_time.message) ? bus.errors.departure_time.message : ''}`}</InputLabel>
+                    <Input
+                        id={'departure-time'}
+                        type={'time'}
+                        name={'departure_time'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.departure_time || ''}
+                        error={bus.errors.departure_time && !!bus.errors.departure_time.message}
+                    />
+                </FormControl>
+                <FormControl className={props.classes.formControl}>
+                    <InputLabel htmlFor="password">{`Price: ${(bus.errors.price && !!bus.errors.price.message) ? bus.errors.price.message : ''}`}</InputLabel>
+                    <Input
+                        id={'price'}
+                        type={'text'}
+                        name={'price'}
+                        onChange={props.handleChange}
+                        onFocus={props.handleFocus}
+                        value={bus.current.price || '0.00'}
+                        error={bus.errors.price && !!bus.errors.price.message}
+                    />
+                </FormControl>
+
                 <FormGroup>
                     <FormControlLabel
                         label={`Is return? ${(bus.errors.is_return && !!bus.errors.is_return.message) ? bus.errors.is_return.message : ''}`}
@@ -77,39 +104,44 @@ const BusForm = ({handleSubmit, handleChange, handleFocus, bus}) => {
                             <Checkbox
                                 error={bus.errors.is_return && !!bus.errors.is_return.message}
                                 checked={!!bus.current.is_return}
-                                onChange={handleChange}
-                                onFocus={handleFocus}
+                                onChange={props.handleChange}
+                                onFocus={props.handleFocus}
                                 value={bus.current.is_return ? '1' : ''}
                                 name="is_return"
                             />
                         }
                     />
                 </FormGroup>
+
                 {!!bus.current.is_return && [
-                    <TextField
-                        error={bus.errors.return_departure_date && !!bus.errors.return_departure_date.message}
-                        helperText={`Return departure date: ${(bus.errors.return_departure_date && !!bus.errors.return_departure_date.message) ? bus.errors.return_departure_date.message : ''}`}
-                        id={'return-departure-date'}
-                        type={'date'}
-                        name={'return_departure_date'}
-                        onChange={handleChange}
-                        onFocus={handleFocus}
-                        value={bus.current.return_departure_date || ''}
-                        key={1}
-                    />,
-                    <TextField
-                        error={bus.errors.return_departure_time && !!bus.errors.return_departure_time.message}
-                        helperText={`Return departure time: ${(bus.errors.return_departure_time && !!bus.errors.return_departure_time.message) ? bus.errors.return_departure_time.message : ''}`}
-                        id={'return-departure-time'}
-                        type={'time'}
-                        name={'return_departure_time'}
-                        onChange={handleChange}
-                        onFocus={handleFocus}
-                        value={bus.current.return_departure_time || ''}
-                        key={2}
-                    />,
+                    <FormControl className={props.classes.formControl} key={1}>
+                        <InputLabel htmlFor="password">{`Return departure date: ${(bus.errors.return_departure_date && !!bus.errors.return_departure_date.message) ? bus.errors.return_departure_date.message : ''}`}</InputLabel>
+                        <Input
+                            id={'return-departure-date'}
+                            type={'date'}
+                            name={'return_departure_date'}
+                            onChange={props.handleChange}
+                            onFocus={props.handleFocus}
+                            value={bus.current.return_departure_date || ''}
+                            error={bus.errors.return_departure_date && !!bus.errors.return_departure_date.message}
+                        />
+                    </FormControl>,
+                    <FormControl className={props.classes.formControl} key={2}>
+                        <InputLabel htmlFor="password">{`Return departure time: ${(bus.errors.return_departure_time && !!bus.errors.return_departure_time.message) ? bus.errors.return_departure_time.message : ''}`}</InputLabel>
+                        <Input
+                            id={'return-departure-time'}
+                            type={'time'}
+                            name={'return_departure_time'}
+                            onChange={props.handleChange}
+                            onFocus={props.handleFocus}
+                            value={bus.current.return_departure_date || ''}
+                            error={bus.errors.return_departure_time && !!bus.errors.return_departure_time.message}
+                        />
+                    </FormControl>,
                 ]}
-                <Button type="submit">Save</Button>
+                <FormControl className={props.classes.formControl}>
+                    <Button raised color="primary" type="submit">Save</Button>
+                </FormControl>
             </FormControl>
         </form>
     );
@@ -120,4 +152,4 @@ BusForm.bookingLabel = 'bus';
 BusForm.newLabel = NEW_BUS;
 BusForm.editLabel = EDIT_BUS;
 
-export default Booking(BusForm);
+export default withStyles(styles)(Booking(BusForm));
