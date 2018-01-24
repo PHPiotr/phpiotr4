4ecbbef874df55c22133d40577095d439c2aa80e
Piotr Kowalski <piet.kowalski@gmail.com>
2018-01-24 20:42:34 +0000

    Refactor - use fragment not array for layout render

--- app/components/containers/App.js
+++ app/components/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import {withRouter, Route, Switch} from 'react-router-dom';
 import Navbar from '../presentation/Navbar';
@@ -46,28 +46,30 @@ class App extends Component {
     }
 
     render() {
-        return [
-            <Navbar key={1}/>,
-            <div key={2} style={{paddingTop: 70}}>
-                <Switch>
-                    <Route exact path="/" component={Report}/>
-                    <Route name="login" path="/login" component={Login}/>
-                    <Route name="register" path="/register/:userId?/:bearerToken?" component={Registration}/>
-                    <Route name="logout" path="/logout" component={Logout}/>
-                    <Route name="buses" path="/bookings/buses/:current?/:page?" component={Buses}/>
-                    <Route name="bus" path="/bookings/bus/:id?" component={Bus}/>
-                    <Route name="planes" path="/bookings/planes/:current?/:page?" component={Planes}/>
-                    <Route name="plane" path="/bookings/plane/:id?" component={Plane}/>
-                    <Route name="trains" path="/bookings/trains/:current?/:page?" component={Trains}/>
-                    <Route name="train" path="/bookings/train/:id?" component={Train}/>
-                    <Route name="hostels" path="/bookings/hostels/:current?/:page?" component={Hostels}/>
-                    <Route name="hostel" path="/bookings/hostel/:id?" component={Hostel}/>
-                    <Route name="account-recovery" path="/account-recovery" component={AccountRecovery}/>
-                    <Route name="password-reset" path="/password-reset/:userId/:token" component={PasswordReset}/>
-                    <Route name="404" path="*" component={NotFound}/>
-                </Switch>
-            </div>,
-        ];
+        return (
+            <Fragment>
+                <Navbar/>
+                <div style={{paddingTop: 70}}>
+                    <Switch>
+                        <Route exact path="/" component={Report}/>
+                        <Route name="login" path="/login" component={Login}/>
+                        <Route name="register" path="/register/:userId?/:bearerToken?" component={Registration}/>
+                        <Route name="logout" path="/logout" component={Logout}/>
+                        <Route name="buses" path="/bookings/buses/:current?/:page?" component={Buses}/>
+                        <Route name="bus" path="/bookings/bus/:id?" component={Bus}/>
+                        <Route name="planes" path="/bookings/planes/:current?/:page?" component={Planes}/>
+                        <Route name="plane" path="/bookings/plane/:id?" component={Plane}/>
+                        <Route name="trains" path="/bookings/trains/:current?/:page?" component={Trains}/>
+                        <Route name="train" path="/bookings/train/:id?" component={Train}/>
+                        <Route name="hostels" path="/bookings/hostels/:current?/:page?" component={Hostels}/>
+                        <Route name="hostel" path="/bookings/hostel/:id?" component={Hostel}/>
+                        <Route name="account-recovery" path="/account-recovery" component={AccountRecovery}/>
+                        <Route name="password-reset" path="/password-reset/:userId/:token" component={PasswordReset}/>
+                        <Route name="404" path="*" component={NotFound}/>
+                    </Switch>
+                </div>
+            </Fragment>
+        );
     }
 }
 
