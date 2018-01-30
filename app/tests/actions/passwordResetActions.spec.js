89c4ae88a77554ad13ffe650005e2c1686aa6efd
Piotr Kowalski <piet.kowalski@gmail.com>
2018-01-30 12:25:54 +0000

    Password Reset - should create RESET_PASSWORD_SUCCESS when password reset done

--- app/tests/actions/passwordResetActions.spec.js
+++ app/tests/actions/passwordResetActions.spec.js
@@ -1,18 +1,40 @@
 import expect from 'expect';
 import * as passwordResetActions from '../../actions/passwordReset/passwordResetActions';
 import * as passwordResetActionTypes from '../../actions/passwordReset/passwordResetActionTypes';
-//import configureMockStore from 'redux-mock-store';
-//import thunk from 'redux-thunk';
-//import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
 import 'isomorphic-fetch';
 import 'babel-polyfill';
 
-//const apiUrl = 'http://localhost:8080';
-//const apiPrefix = '/api/v1';
-//const middlewares = [thunk];
-//const mockStore = configureMockStore(middlewares);
+const apiUrl = 'http://localhost:8080';
+const apiPrefix = '/api/v1';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Password Reset Actions', () => {
+    it(`should create ${passwordResetActionTypes.RESET_PASSWORD_SUCCESS} when password reset done`, () => {
+        const password = '1Qwertyuiop2@';
+        const store = mockStore({
+            passwordReset: {
+                isResetting: false,
+                password,
+                repeatPassword: password,
+            },
+        });
+        const userId = 1;
+        const token = 'j.w.t';
+        nock(apiUrl).patch(`${apiPrefix}/users/${userId}`).reply(204);
+        const expectedActions = [
+            {type: passwordResetActionTypes.RESET_PASSWORD_REQUEST},
+            {type: passwordResetActionTypes.RESET_PASSWORD_SUCCESS},
+        ];
+        return store.dispatch(passwordResetActions.resetPasswordIfNeeded(userId, token, {
+            newPassword: password,
+            newPasswordRepeat: password,
+        }))
+            .then(() => expect(store.getActions()).toEqual(expectedActions));
+    });
     it(`should create ${passwordResetActionTypes.SET_RESET_PASSWORD_INPUT_VALUE} when password input value set`, () => {
         const payload = 'qwerty';
         const expectedAction = {
