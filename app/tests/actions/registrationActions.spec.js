77f96be48bd4216156509a643ae952333a3f3120
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-01 13:25:13 +0000

    Registration actions - should create REGISTRATION_SUCCESS when registration succeeds

--- app/tests/actions/registrationActions.spec.js
+++ app/tests/actions/registrationActions.spec.js
@@ -1,8 +1,47 @@
 import expect from 'expect';
 import * as registrationActions from '../../actions/registration/registrationActions';
 import * as registrationActionTypes from '../../actions/registration/registrationActionTypes';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import 'isomorphic-fetch';
+import 'babel-polyfill';
+
+const apiUrl = 'http://localhost:8080';
+const apiPrefix = '/api/v1';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Registration Actions', () => {
+    it(`should create ${registrationActionTypes.REGISTRATION_SUCCESS} when registration succeeds`, () => {
+        const store = mockStore({
+            registration: {
+                registration: {
+                    username: 'hello',
+                    email: 'hello@example.com',
+                    password: '1Qwertyuiop2@',
+                    repeatPassword: '1Qwertyuiop2@',
+
+                },
+            },
+            auth: {
+                isLoggingIn: false,
+                isLoggedIn: false,
+                activationUrl: 'http://www.example.com',
+                activationFromEmail: 'user@example.com',
+            },
+        });
+        nock(apiUrl).post(`${apiPrefix}/users`).reply(201);
+        const expectedActions = [
+            {type: registrationActionTypes.REGISTRATION_REQUEST},
+            {
+                type: registrationActionTypes.REGISTRATION_SUCCESS,
+                payload: 'Account created. We have sent you an email with activation instructions.',
+            },
+        ];
+        return store.dispatch(registrationActions.registerIfNeeded())
+            .then(() => expect(store.getActions()).toEqual(expectedActions));
+    });
     it(`should create ${registrationActionTypes.ON_CHANGE_REGISTRATION_FIELD} on input change`, () => {
         const fieldName = 'password';
         const fieldValue = '1Qwertyuiop2@';
