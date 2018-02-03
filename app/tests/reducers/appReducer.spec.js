273ed73f174a304b51756492a5048b67fdb6e3d9
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-03 19:29:54 +0000

    App reducer - should set SET_APP_BAR_TITLE action type when app bar title to be set

--- app/tests/reducers/appReducer.spec.js
+++ app/tests/reducers/appReducer.spec.js
@@ -2,6 +2,7 @@ import app from '../../reducers/appReducer';
 import * as appActionTypes from '../../actions/app/appActionTypes';
 import expect from 'expect';
 import freeze from 'deep-freeze';
+import {HOME} from '../../constants';
 
 describe('App reducer', () => {
     it(`should set ${appActionTypes.TOGGLE_IS_DRAWER_OPEN} action type when drawer to be toggled on`, () => {
@@ -17,6 +18,26 @@ describe('App reducer', () => {
         const action = {type: appActionTypes.TOGGLE_IS_DRAWER_OPEN};
         const before = {isDrawerOpen: true};
         const after = {isDrawerOpen: false};
+        freeze(action);
+        freeze(before);
+        freeze(after);
+        expect(app(before, action)).toEqual(after);
+    });
+    it(`should set ${appActionTypes.SET_APP_BAR_TITLE} action type when app bar title to be set`, () => {
+        const payload = 'Expected app bar title';
+        const action = {type: appActionTypes.SET_APP_BAR_TITLE, payload};
+        const before = {appBarTitle: ''};
+        const after = {appBarTitle: payload};
+        freeze(action);
+        freeze(before);
+        freeze(after);
+        expect(app(before, action)).toEqual(after);
+    });
+    it(`should set ${appActionTypes.SET_APP_BAR_TITLE} action type when app bar title to be set to default`, () => {
+        const defaultAppBarTitle = HOME;
+        const action = {type: appActionTypes.SET_APP_BAR_TITLE};
+        const before = {appBarTitle: defaultAppBarTitle};
+        const after = {appBarTitle: defaultAppBarTitle};
         freeze(action);
         freeze(before);
         freeze(after);
