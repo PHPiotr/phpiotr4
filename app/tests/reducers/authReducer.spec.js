a89dd2597dd3dd2ae7d038be33da519d874e1332
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-04 12:13:25 +0000

    Auth reducer - should change isLoggedIn flag to true in case of authActionTypes.LOGIN_SUCCESS action type

--- app/tests/reducers/authReducer.spec.js
+++ app/tests/reducers/authReducer.spec.js
@@ -4,7 +4,7 @@ import * as authActionTypes from '../../actions/auth/authActionTypes';
 import freeze from 'deep-freeze';
 
 describe('Auth reducer', () => {
-    it(`should set isLoggedIn flag to true in case of ${authActionTypes.LOGIN_REQUEST} action type`, () => {
+    it(`should set isLoggingIn flag to true in case of ${authActionTypes.LOGIN_REQUEST} action type`, () => {
         const action = {type: authActionTypes.LOGIN_REQUEST};
         const before = {isLoggingIn: false};
         const after = {isLoggingIn: true};
@@ -13,10 +13,20 @@ describe('Auth reducer', () => {
         freeze(after);
         expect(auth(before, action)).toEqual(after);
     });
-    it(`should not change isLoggedIn flag if it is already true in case of ${authActionTypes.LOGIN_REQUEST} action type`, () => {
+    it(`should not change isLoggingIn flag if it is already true in case of ${authActionTypes.LOGIN_REQUEST} action type`, () => {
         const action = {type: authActionTypes.LOGIN_REQUEST};
         const before = {isLoggingIn: true};
         const after = {isLoggingIn: true};
+        freeze(action);
+        freeze(before);
+        freeze(after);
+        expect(auth(before, action)).toEqual(after);
+    });
+    it(`should change isLoggedIn flag to true in case of ${authActionTypes.LOGIN_SUCCESS} action type`, () => {
+        const payload = {token: 'j.w.t', expiresIn: Date()};
+        const action = {type: authActionTypes.LOGIN_SUCCESS, payload};
+        const before = {isLoggingIn: true, isLoggedIn: false, token: '', expiresIn: 0};
+        const after = {...payload, isLoggingIn: false, isLoggedIn: true, login: {}};
         freeze(action);
         freeze(before);
         freeze(after);
