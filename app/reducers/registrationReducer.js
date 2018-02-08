38229e23f4d0d1740f665cce9f08b83501cf65ab
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-08 13:23:53 +0000

    Registration - refactor reducer

--- app/reducers/registrationReducer.js
+++ app/reducers/registrationReducer.js
@@ -25,8 +25,8 @@ const registration = (state = initialState, action) => {
             return {
                 ...state,
                 isRegistering: false,
-                registrationErrorMessage: action.payload.error || '',
-                registrationErrors: action.payload.errors || {},
+                registrationErrorMessage: action.payload.error,
+                registrationErrors: action.payload.errors,
             };
         case registrationActionTypes.ON_FOCUS_REGISTRATION_FIELD:
             return {
