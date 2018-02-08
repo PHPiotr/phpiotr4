a1067646fcec6f2dfb193a891f1074f63f764246
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-08 09:28:45 +0000

    Registration - should succeed request on registrationActionTypes.REGISTRATION_FAILURE

--- app/tests/reducers/registrationReducer.spec.js
+++ app/tests/reducers/registrationReducer.spec.js
@@ -16,8 +16,23 @@ describe('Registration', () => {
     it(`should succeed request on ${registrationActionTypes.REGISTRATION_SUCCESS}`, () => {
         const payload = 'Success';
         const action = {type: registrationActionTypes.REGISTRATION_SUCCESS, payload};
-        const before = {isRegistering: true, registrationSuccessMessage: payload, registration: {hello: 'world'}};
+        const before = {isRegistering: true, registrationSuccessMessage: '', registration: {hello: 'world'}};
         const after = {isRegistering: false, registrationSuccessMessage: payload, registration: {}};
+        freeze(action);
+        freeze(before);
+        freeze(after);
+        expect(registration(before, action)).toEqual(after);
+    });
+    it(`should succeed request on ${registrationActionTypes.REGISTRATION_FAILURE}`, () => {
+        const payload = {
+            error: 'Some error',
+            errors: {
+                whatever: true,
+            },
+        };
+        const action = {type: registrationActionTypes.REGISTRATION_FAILURE, payload};
+        const before = {isRegistering: true, registrationErrorMessage: '', registrationErrors: {}};
+        const after = {isRegistering: false, registrationErrorMessage: payload.error, registrationErrors: payload.errors};
         freeze(action);
         freeze(before);
         freeze(after);
