199886eea095c22d082032c94dbba5581d455374
Piotr Kowalski <piet.kowalski@gmail.com>
2018-02-28 22:43:44 +0000

    Experimenting with webpack 4

--- webpack/server/webpack.server.dev.js
+++ webpack/server/webpack.server.dev.js
@@ -1,13 +1,6 @@
-const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.server.common.js');
 
 module.exports = merge(common, {
-    plugins: [
-        new Webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development'),
-            },
-        }),
-    ],
+    mode: 'development',
 });
